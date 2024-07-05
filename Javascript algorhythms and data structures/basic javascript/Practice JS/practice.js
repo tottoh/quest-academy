fa3929555c391381227a9e72943238ad717a4131
tottoh <tottocheng@gmail.com>
2024-07-05 16:21:19 +0700

    cooking

---
+++ Javascript algorhythms and data structures/basic javascript/Practice JS/practice.js
@@ -0,0 +1,64 @@
+var taskList = [];
+
+function displayTasks() {
+    console.log("Tasks List:");
+    if (taskList.length === 0) {
+        console.log("No tasks to display");
+    }
+    if (taskList.length >= 1) {
+        console.log(`1. ${taskList[0]}`);
+    }
+    if (taskList.length >= 2) {
+        console.log(`2. ${taskList[1]}`);
+    }
+    if (taskList.length >= 3) {
+        console.log(`3. ${taskList[2]}`);
+    }
+    if (taskList.length >= 4) {
+        console.log(`4. ${taskList[3]}`);
+    }
+    if (taskList.length >= 5) {
+        console.log(`5. ${taskList[4]}`);
+    }
+console.log("Total tasks:", taskList.length);
+}
+displayTasks();
+
+
+/*
+function addtask() {
+    tasklist.push("Coding");
+}
+function removeTask{
+    tasklist.shift("Coding");
+    let removedTask = tasks.shift()
+}
+tasklist.length
+console.log(tasklist);
+
+if (){
+    console.log(tasklist;)
+}
+if () {
+    console.log(1. Math Homework)
+}
+
+Tasks List:
+1. Math Homework
+2. Science Project
+3. English Essay
+Total tasks: 3
+Added new task: History Assignment
+Tasks List:
+1. Math Homework
+2. Science Project
+3. English Essay
+4. History Assignment
+Total tasks: 4
+Removed task: Math Homework
+Tasks List:
+1. Science Project
+2. English Essay
+3. History Assignment
+Total tasks: 3
+*/
